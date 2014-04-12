6d030a853a079be07315b8bd82a1e208ac3ba151
Ethan Fischer <efische2@iwu.edu>
2014-04-12 16:13:49 -0500

    Changed the output message after user uploads databse

--- database_uploadform.php
+++ database_uploadform.php
@@ -90,29 +90,15 @@
   		  }
  		 else
  		   {
- 		   echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-  	  	echo "Type: " . $_FILES["file"]["type"] . "<br>";
-  	 	 echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-  	 	 echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
-    
-
-
-   	 	if (file_exists("upload/" . $_FILES["file"]["name"]))
-   	  	 {
-   	  	 echo $_FILES["file"]["name"] . " already exists. ";
-   	 	  }
-  		  else
-   		   {
    		   move_uploaded_file($_FILES["file"]["tmp_name"],
-   		   "upload/" . $_FILES["file"]["name"]);
-   		   echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
-   		   echo "<br> File Uploded Succesfully";
-   		   }
+   		   "upload/database.txt");
+   		   //echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+   		   echo "<br> Your database has been successflly uploded";
   		  }
  		 }
 		else
  		 {
- 		 echo "Invalid file! Have a nice day.";
+ 		 echo "Invalid file. Have a nice day!";
  		 }
  	 } 
 	?>
