a47eb0452da543115f6469cbc74442443b394a24
Jeremy Albert <jalbert1@acm-server.iwu.edu>
2014-04-12 19:15:21 -0500

    front end javascript filtering mostly complete, must handle groups and unavailable variable selections

---
+++ js/site.js
@@ -0,0 +1,85 @@
+//Matt and Jeremey Front End Database JavaScript
+
+//retrieve database
+//changes values on click
+$("#left_ear button, #right_ear button").click(function( event ) {
+	if ($(this).text()== "0") {
+	  $(this).text("1");
+	}
+	else{
+	  $(this).text("0");
+	}
+	event.preventDefault();
+});
+
+$("#male").click(function( event ) {
+	$("#female, #unavailable").removeClass("active");
+	$(this).addClass("active");
+	event.preventDefault();
+});
+$("#female").click(function( event ) {
+	$("#male, #unavailable").removeClass("active");
+	$(this).addClass("active");
+	event.preventDefault();
+});
+$("#unavailable").click(function( event ) {
+	$("#male, #female").removeClass("active");
+	$(this).addClass("active");
+	event.preventDefault();
+});
+
+$("#left_ear button").click(function( event ) {
+	$("#know button").removeClass("active");
+});
+$("#right_ear button").click(function( event ) {
+	$("#know1 button").removeClass("active");
+});
+$("#know button").click(function( event ) {
+	$("#left_ear button").removeClass("active");
+	$("#left_ear button").text("0");
+});
+$("#know1 button").click(function( event ) {
+	$("#right_ear button").removeClass("active");
+	$("#right_ear button").text("0");
+});
+
+
+
+//JSON string parsing
+var monkeys;
+$.getJSON("get_database.php", function(data) { 
+	monkeys = data;
+	build_table()
+})
+
+var rows = {};
+function build_table(){
+	for(var i = 0; i < monkeys.length; i++){
+		rows[i] = $('<tr><td>' + monkeys[i].id + '</td><td>' + monkeys[i].sex + '</td><td>'+ monkeys[i].birthYear +'</td><td>' + monkeys[i].leftEar + '</td><td>' + monkeys[i].rightEar + '</td><td>' + monkeys[i].notes + '</td></tr>');
+		$('#table1 tbody').append(rows[i]);
+	}
+}
+
+$("#form1 button").click(update_table);
+
+$("#form1 input").change(update_table);
+
+function update_table() {
+	var left_ear = $("#left_ear1").text() + $("#left_ear2").text() + $("#left_ear3").text();
+	var right_ear = $("#right_ear1").text() + $("#right_ear2").text() + $("#right_ear3").text();
+	var gender = $("#gender .active").text(); 
+	
+	for(var i = 0; i < monkeys.length; i++){
+	
+	//if(left_ear == 'Don\'t Know' || right_ear == 'Don\'t Know' || gender == 'Unavailable'){
+			if(monkeys[i].sex != gender || monkeys[i].rightEar != right_ear || monkeys[i].leftEar != left_ear){
+					rows[i].remove();
+				}
+				else{
+					rows[i].appendTo("#table1 tbody");
+				}
+			}
+	//}
+}
+
+
